10dd5d535fb1f2e6bea4afafb254bc6da8382aa8
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2020-04-10 15:02:07 -0300

    creating methods for return string filters

--- app/src/main/java/com/example/avadakedavra/model/GetData.java
+++ app/src/main/java/com/example/avadakedavra/model/GetData.java
@@ -1,6 +1,5 @@
 package com.example.avadakedavra.model;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
--- app/src/main/java/com/example/avadakedavra/view/activitys/MainActivity.java
+++ app/src/main/java/com/example/avadakedavra/view/activitys/MainActivity.java
@@ -8,10 +8,31 @@ import com.example.avadakedavra.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String houseFilter;
+    private boolean hogwartsStudentsOnly;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String getHouseFilter(){
+        return getString(R.string.house) + this.houseFilter;
+    }
+
+    public boolean getHogwartsStudentsOnly(){
+        return this.hogwartsStudentsOnly;
+    }
+
+    public void setHogwartsStudentsOnly(boolean hogwartsStudentsOnly){
+        this.hogwartsStudentsOnly = hogwartsStudentsOnly;
+    }
+
+    public String getStringOnlyStudents(){
+        return hogwartsStudentsOnly
+                ? getString(R.string.onlyStudents) + getString(R.string.yes)
+                : getString(R.string.onlyStudents) + getString(R.string.no);
     }
 
 }
